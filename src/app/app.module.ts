c27a33fbc725c4d1379c36bc6e887478794d8093
Filip M <filipm93@gmail.com>
2017-08-30 22:02:19 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeroesComponent,
+		DashboardComponent,
+		HeroDetailComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule
+	],
+	providers: [HeroService],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
